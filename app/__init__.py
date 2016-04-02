92322a20acafe179903125da7fef2dd700788928
turbo_jesus <sirmaahe@gmail.com>
2016-04-02 18:36:04 +0300

    Integrate w/ gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask.ext.pymongo import PyMongo
+from werkzeug.contrib.fixers import ProxyFix
 
 
 app = Flask(__name__)
-app.secret_key = '2'
+app.secret_key = 'SECRET KEY'
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 mongo = PyMongo(app)
 
