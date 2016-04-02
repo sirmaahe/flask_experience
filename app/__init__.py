d4a44d8779aedffcd5cc4dfebddbf8812f22289e
turbo_jesus <sirmaahe@gmail.com>
2016-04-02 21:55:50 +0300

    More settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
+import os
+
 from flask import Flask
 from flask.ext.pymongo import PyMongo
 from werkzeug.contrib.fixers import ProxyFix
 
+MONGO_URL = os.environ.get('MONGO_URL')
+if not MONGO_URL:
+    MONGO_URL = "mongodb://localhost:27017/rest"
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = MONGO_URL
 app.secret_key = 'SECRET KEY'
 app.debug = True
 app.wsgi_app = ProxyFix(app.wsgi_app)
